bd3cc55eecc5b250634bf546bf3fdd7289af0ca8
Carlos Sheldon <83865397+Carl98BR@users.noreply.github.com>
2023-11-01 14:48:51 -0300

    Update gray2bin.py

--- pycommtools/utilityfunctions/gray2bin.py
+++ pycommtools/utilityfunctions/gray2bin.py
@@ -1,1 +1,19 @@
+import numpy as np
 
+def gray2bin(gray_matrix, msbf=True):
+  binary_matrix = np.zeros_like(gray_matrix)
+  
+  # Handle the case where gray_matrix is 1D (a single binary number)
+  if len(gray_matrix.shape) == 1:
+    binary_matrix[0] = gray_matrix[0]
+    for i in range(1, gray_matrix.shape[0]):
+      binary_matrix[i] = np.bitwise_xor(binary_matrix[i - 1], gray_matrix[i])
+  else:
+    binary_matrix[:, 0] = gray_matrix[:, 0]
+    for i in range(1, gray_matrix.shape[1]):
+      binary_matrix[:, i] = np.bitwise_xor(binary_matrix[:, i - 1], gray_matrix[:, i])
+
+  if not msbf:
+    binary_matrix = np.flip(binary_matrix, axis=-1)
+
+  return binary_matrix

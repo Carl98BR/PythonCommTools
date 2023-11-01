5edcb03b47deec895576faaf3e9dc549e2442554
Carlos Sheldon <83865397+Carl98BR@users.noreply.github.com>
2023-10-31 21:38:14 -0300

    Update awgn.py

--- Random Signals/awgn.py
+++ Random Signals/awgn.py
@@ -1,1 +1,27 @@
+import numpy as np
 
+def awgn(x, snr, pwr=None, seed=None, powertype="dB"):
+  # If pwr is not specified, set it to 0
+  if pwr is None:
+    pwr = 0
+  
+  # If pwr is set to "measured", calculate the power from the input signal x
+  if pwr == "measured":
+    pwr = np.mean(np.abs(x)**2)
+    if powertype == "dB":
+      pwr = 10 * np.log10(pwr)
+  
+  # Calculate the noise power based on SNR and power type
+  if powertype == "linear":
+    noise_power = pwr / snr
+  else:
+    noise_power = pwr - snr
+  
+  # Generate complex or real-valued noise based on the input signal x
+  if np.iscomplexobj(x):
+    noise = wgn(x.shape[0], x.shape[1], noise_power, 1, seed, powertype, output="complex")
+  else:
+    noise = wgn(x.shape[0], x.shape[1], noise_power, 1, seed, powertype, output="real")
+  
+  # Add generated noise to the input signal x
+  return x + noise

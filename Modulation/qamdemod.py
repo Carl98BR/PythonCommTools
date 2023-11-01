97e2b48a2ddec6fb9ce8b505c25b60496ba73804
Carlos Sheldon <83865397+Carl98BR@users.noreply.github.com>
2023-10-31 21:25:18 -0300

    Update qamdemod.py

--- Modulation/qamdemod.py
+++ Modulation/qamdemod.py
@@ -1,1 +1,13 @@
+import numpy as np
+from scipy.spatial.distance import cdist
 
+def qamdemod(modulated_signal, M, symorder='gray'):
+    # Create a QAM constellation based on the specified 'symorder'
+    constellation = qammod(np.arange(0, M), M, symorder)
+
+    # Extract the real and imaginary parts of the constellation and modulated signals
+    constellation = np.column_stack((np.real(constellation), np.imag(constellation)))
+    modulated_signal = np.column_stack((np.real(modulated_signal), np.imag(modulated_signal)))
+
+    # Calculate the Euclidean distance between each modulated point and constellation points
+    return np.argmin(cdist(modulated_signal, constellation, metric='euclidean'), axis=1)

b57c9976ca57c0b2f5f6c998f4929c3b358a32d7
Carlos Sheldon <83865397+Carl98BR@users.noreply.github.com>
2023-11-14 07:42:23 -0300

    Update berawgn.py

--- pycommtools/dataanalysis/berawgn.py
+++ pycommtools/dataanalysis/berawgn.py
@@ -1,7 +1,12 @@
 from PythonCommTools.pycommtools.utilityfunctions import *
 import numpy as np
-
 def berawgn(EbNo, modtype, M):
+  def Pb(k, gamma, M):
+    return 1 / np.sqrt(M) * np.sum([w(i, k, M) * erfc((2 * i + 1) * np.sqrt(3 / 2 * np.log2(M) * gamma / (M - 1))) for i in range(int((1 - 2 ** (-k)) * np.sqrt(M)))])
+  
+  def w(i, k, M):
+    return (-1) ** np.floor(i * 2 ** (k - 1) / np.sqrt(M)) * (2 ** (k - 1) - np.floor(i * 2 ** (k - 1) / np.sqrt(M) + 1 / 2))
+    
   gamma_b = 10 ** (EbNo / 10)
   
   if modtype.upper() in ["BFSK", "BPSK", "QPSK"]:
@@ -17,9 +22,8 @@ def berawgn(EbNo, modtype, M):
     h2 = np.sin(np.pi / M)
     g = 2 * gamma_b * np.log2(M)
   elif modtype.upper() == "QAM":
-    h1 = 4 / np.log2(M) * (1 - 1 / np.sqrt(M))
-    h2 = 1
-    g = 3 * gamma_b * np.log2(M) / (M - 1)
+    result = np.vectorize(lambda x: 1 / np.log2(np.sqrt(M)) * np.sum([Pb(k, x, M) for k in range(1, int(np.log2(np.sqrt(M))) + 1)]))
+    return result(gamma_b)
   else:
     raise ValueError("BERAWGN: Invalid modulation type")
 

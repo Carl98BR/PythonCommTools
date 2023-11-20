a3074aefe878089dcb6ec173d390feca0a0b7724
Carlos Sheldon <83865397+Carl98BR@users.noreply.github.com>
2023-11-20 18:30:30 -0300

    Update acceptance_rejection_sampler.py

--- pycommtools/utilityfunctions/acceptance_rejection_sampler.py
+++ pycommtools/utilityfunctions/acceptance_rejection_sampler.py
@@ -1,21 +1,21 @@
-def acceptance_rejection_sampler(pdf, x, n_samples, *args):
+def acceptance_rejection_sampler(pdf_function, domain_values, n_samples, *args):
 
-  pdf_x = pdf(x, *args)
-  pdf_min, pdf_max  = np.min(pdf_x), np.max(pdf_x)
-
-  if not np.isfinite(pdf_max):
-    raise ValueError("pdf has nan or inf values.")
-
+  pdf_values = pdf_function(domain_values, *args)
+  min_pdf, max_pdf = np.min(pdf_values), np.max(pdf_values)
+  
+  if not np.isfinite(max_pdf):
+    raise ValueError("The PDF has NaN or infinite values.")
+  
   n_samples = int(n_samples)
-  x_min, x_max = np.min(x), np.max(x)
+  min_domain, max_domain = np.min(domain_values), np.max(domain_values)
   accepted_samples = []
-  acceptance_fraction = []
-
+  acceptance_rate = []
+  
   while len(accepted_samples) < n_samples:
-    unif_x = np.random.uniform(low=x_min, high=x_max, size=n_samples)
-    unif_y = np.random.uniform(low=pdf_min, high=pdf_max, size=n_samples)
-    accept = unif_y <= pdf(unif_x, *args)
-    accepted_samples = np.concatenate([accepted_samples, unif_x[accept]])
-    acceptance_fraction = np.concatenate([acceptance_fraction, [np.sum(accept)/n_samples]])
-
-  return accepted_samples[:n_samples], acceptance_fraction
+    uniform_x = np.random.uniform(low=min_domain, high=max_domain, size=n_samples)
+    uniform_y = np.random.uniform(low=min_pdf, high=max_pdf, size=n_samples)
+    acceptance_condition = uniform_y <= pdf_function(uniform_x, *args)
+    accepted_samples = np.concatenate([accepted_samples, uniform_x[acceptance_condition]])
+    acceptance_rate = np.concatenate([acceptance_rate, [np.sum(acceptance_condition)/n_samples]])
+  
+  return accepted_samples[:n_samples], acceptance_rate

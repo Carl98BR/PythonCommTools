6ed548ae650f0cecfe0cb4cc5710a30224a7ef46
Carlos Sheldon <83865397+Carl98BR@users.noreply.github.com>
2023-11-22 11:07:11 -0300

    Update __init__.py

--- pycommtools/utilityfunctions/__init__.py
+++ pycommtools/utilityfunctions/__init__.py
@@ -4,5 +4,7 @@ from .int2bin import *
 from .gray2bin import *
 from .qfunc import *
 from .convertSNR import *
+from .acceptance_rejection_sampler import *
+from .distributions import *
 
-__all__ = ['bin2gray', 'bin2int', 'int2bin', 'gray2bin', 'qfunc', 'convertSNR']
+__all__ = ['bin2gray', 'bin2int', 'int2bin', 'gray2bin', 'qfunc', 'convertSNR', 'acceptance_rejection_sampler', 'distributions']
